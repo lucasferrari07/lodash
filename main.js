c60457374b2d550799ba82e035b0c1cc8ced34b1
Lucas Ferrari da Costa <lucas.ferrari.costa@gmail.com>
2019-04-10 08:45:58 -0300

    Implement reduce function
    - Implemented reduce function and unit tests for it.
    - Reduce function takes 3 arguments (iterable, reducer and
    initialValue). Converts iterable to an array if it's not.
    - Throws an TypeError if the reducer argument is not a function.
    - If the initialValue is set, it is initially stored in the accumulator,
    which stores the value of the execution of the function passed in the
    reducer argument. If initialValue is undefined, the first value in the
    iterable array is store in the accumulator.
    - Then, for each value in the array, the reducer function is executed,
    it takes 3 arguments (an accumulator, the currentValue in the array and
    it's currentIndex);
    - After the for loop, the accumulator is returned.

--- main.js
+++ main.js
@@ -7,7 +7,20 @@ const _ = {
     }
     return newIterable;
   },
+  reduce(iterable, reducer, initialValue) {
+    if (typeof reducer !== 'function') {
+      throw new TypeError('\'reducer\' argument should be a function');
+    }
+    const array = Array.isArray(iterable) ? iterable : Array.of(iterable);
+    const initialIndex = typeof initialValue === 'undefined' ? 1 : 0;
+    let [accumulator] = typeof initialValue === 'undefined' ? array : [initialValue];
+    for (const [currentIndex, currentValue] of array.entries()) {
+      if (currentIndex >= initialIndex) {
+        accumulator = reducer(accumulator, currentValue, currentIndex);
+      }
+    }
+    return accumulator;
+  },
 };
 
-// eslint-disable-next-line import/prefer-default-export
 module.exports = _;
